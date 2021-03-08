0e8f5949ed209ea49c30c3b98d5bac2529c929b1
Nicole Renée Hubbard <code@nicole.dev>
2021-03-08 14:10:15 -0600

    Update sshd

--- sshd/entrypoint.sh
+++ sshd/entrypoint.sh
@@ -9,5 +9,7 @@ wget https://github.com/${GITHUB_USER}.keys -O /home/$SSH_USER/.ssh/authorized_k
 chown -R $SSH_USER:$SSH_USER /home/$SSH_USER/.ssh
 chmod 700 /home/$SSH_USER/.ssh
 chmod 600 /home/$SSH_USER/.ssh/authorized_keys
+echo "Defaults:$SSH_USER   env_keep += \"HOME\"" > /etc/sudoers.d/$SSH_USER
+echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$SSH_USER
 
 exec "$@"
